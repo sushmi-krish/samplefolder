2cd404e0ad2a7dc2a3dc315915875a8b8fba50b5
sushmi-krish <sushmithakrishnamoorthy@gmail.com>
2025-01-23 17:33:46 -0500

    created counterFollower

---
+++ followersCount/followers_count.js
@@ -0,0 +1,22 @@
+let count = 0;//Initialize count to 0
+function IncreaseCount(){
+    count++ //Increment the count by 1
+}
+function displayCount(){
+    document.getElementById('countDisplay').innerHTML=count;
+}
+function increaseCount(){
+    count++;//Increment the count by 1
+    displayCount(); //Display the count
+    checkCountValue();//check count value and didplay message 
+}
+function checkCountValue(){
+    if(count === 10){
+        alert("Your Instagram post gained 10 followers!Congtatulations!");
+    }
+else if(count === 20)
+{
+        alert("Your Instagram post gained 20 followers!keep it up !");
+
+    }
+}
