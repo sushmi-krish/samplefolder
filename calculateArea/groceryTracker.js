9218dd9ab22baa34d349c6fa77b33d272ffd2fc4
sushmi-krish <sushmithakrishnamoorthy@gmail.com>
2025-01-23 12:19:45 -0500

    added grocery_Tracker file

---
+++ calculateArea/groceryTracker.js
@@ -0,0 +1,10 @@
+let grocery1;
+let grocery2;
+let grocery3;
+function totalAmount(){
+grocery1 = parseFloat(document.getElementById("grocery1").value );
+grocery2 = parseFloat(document.getElementById("grocery2").value );
+grocery3 =parseFloat(document.getElementById("grocery3").value) ;
+let total = grocery1+grocery2+grocery3
+document.getElementById("result").innerHTML =`The total Amount is :$ ${total}`;
+}
