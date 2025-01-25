5d3c1a02a2b90992feef7c20b20e507162da10ba
sushmi-krish <sushmithakrishnamoorthy@gmail.com>
2025-01-25 09:21:49 -0500

    modified Dietary Service

--- controlFLow/Dietary_service.js
+++ controlFLow/Dietary_service.js
@@ -1,4 +1,4 @@
-let Dietary_service = prompt("Star the letter with block")
+let Dietary_service = prompt(`Enter your choice "Employee","Enrolled Member","Subscriber" or "Unsubscriber`)
 //console.log(Dietary_service)
 let userCategory;
 switch(Dietary_service){
@@ -11,8 +11,11 @@ switch(Dietary_service){
     case "Subscriber":
             userCategory ="Partial access to faciliate 'Dietary service'";
         break;
+    case  "Unsubscriber":
+           userCategory ="Subscribe frist to avail this facility"
+        break;
     default:
-            userCategory ="Subscribe frist to avail this facility"
+            userCategory ="please choose the option from give choice."
         break;
 }
 console.log("User Category:",userCategory);
