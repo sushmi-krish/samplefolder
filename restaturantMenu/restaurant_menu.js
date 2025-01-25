1fcee217eb141118297c52a2950b5fee9f959e2c
sushmi-krish <sushmithakrishnamoorthy@gmail.com>
2025-01-25 09:28:06 -0500

    added price to item

--- restaturantMenu/restaurant_menu.js
+++ restaturantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu =['Pancakes','Eggs Benedict','Oatmeal','Frittata']
-const mainCourseMenu =['steak','Pasta','Burger','Salmon'];
-const dessertMenu =['Cake','Ice Cream','Pudding','Fruit Salad'];
+const breakfastMenu =['Pancakes- $12','Eggs Benedict-$22.99','Oatmeal-$21.99','Frittata-$15']
+const mainCourseMenu =['steak-$20.01','Pasta-$9.99','Burger-$19.89','Salmon-$24.43'];
+const dessertMenu =['Cake-$2.99','Ice Cream-$3.99','Pudding-$5.01','Fruit Salad-$4.43'];
 const breakfastMenuItems = breakfastMenu.map((item,index) => `<p> Item ${index+1}: ${item}</p>`).join('');
  document.getElementById('breakFastMenuItems').innerHTML = breakfastMenuItems;
 let mainCourseItem ='';
