a0a74b77f13909998b5b9e521e6e0b3b4591775c
sushmi-krish <sushmithakrishnamoorthy@gmail.com>
2025-01-26 11:24:30 -0500

    added specialization

--- employeeDetails/employee_Details.js
+++ employeeDetails/employee_Details.js
@@ -1,12 +1,12 @@
 const employees = [
-    {id: 1 , name : 'John Doe',age:30,department:'IT',salary:50000},
-    {id: 2 , name : 'Alice Smith',age:28,department:'HR',salary:45000},
-    {id: 3 , name : 'Bob Johnson',age:35,department:'Finance',salary:60000},
-    {id: 4, name : 'Peter Jackson',age:32,department:'HR',salary:52000},
-    {id: 3 , name : 'David Samuvel',age:38,department:'Finance',salary:46000},
+    {id: 1 , name : 'John Doe',age:30,department:'IT',salary:50000, speciaization:'Javascript'},
+    {id: 2 , name : 'Alice Smith',age:28,department:'HR',salary:45000,speciaization:'Python'},
+    {id: 3 , name : 'Bob Johnson',age:35,department:'Finance',salary:60000,speciaization:'Java'},
+    {id: 4, name : 'Peter Jackson',age:32,department:'HR',salary:52000,speciaization:'Python'},
+    {id: 5 , name : 'David Samuvel',age:38,department:'Finance',salary:46000,speciaization:'Javascript'},
     ];
     //function to display all employess
-    const totalEmployees = employees.map((employee,index) => `<p>${employee.id}: ${employee.name} - ${employee.age} - ${employee.department} - $${employee.salary}</p>`).join(' ');
+    const totalEmployees = employees.map((employee,index) => `<p>${employee.id}: ${employee.name} - ${employee.age} - ${employee.department} - $${employee.salary}-${employee.speciaization}</p>`).join(' ');
     document.getElementById('employeeDetails').innerHTML = totalEmployees;
     //function to calculateTotal salaries
     function calculateTotalSalaries(){
@@ -30,4 +30,11 @@ const employees = [
     {
         document.getElementById('employeeDetails').innerHTML =`no employee has been found with this ID`;
     }
+ }
+ //function to display employee details based on speciaization
+ function findSpeciazation(specialty){
+    const foundSpeciazation = employees.filter((employee) => employee.speciaization === specialty)
+    console.log(foundSpeciazation)
+   let spEmployee = foundSpeciazation.map((employee,index) =>`<p>${index+1}:${employee.name}-${employee.department}-${employee.speciaization} </p>`).join(' ');
+   document.getElementById('employeeDetails').innerHTML = spEmployee;
  }
