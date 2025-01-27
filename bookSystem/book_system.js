afca9a689da10864f18897531ae52e7f55ac0cca
sushmi-krish <sushmithakrishnamoorthy@gmail.com>
2025-01-27 09:00:26 -0500

    updated delete function

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -13,7 +13,7 @@ function addBook(){
             bookDescription : bookDescription,
             pagesNumber : pagesNumber
         };
-        books.push(book);
+       books.push(book);
         showbooks();
         clearInputs();
     }
@@ -21,13 +21,26 @@ function addBook(){
         alert(`Please fill in all fields correctly.`)    
     }
 }
+
 function showbooks(){
-    const booksDiv = books.map((book,index) => `<h1>book Number ${index+1} <p> <strong>Book Name : </strong> ${book.name}</p> <p><strong>Author Name:</strong> ${book.authorName}</p><p><strong>Book Description : </strong> ${book.bookDescription}</p><p><strong>No.of pages : </strong> ${book.pagesNumber} mins `);
-    document.getElementById('books').innerHTML = booksDiv.join(' ');
+     const booksDiv = books.map((book,index) =>  {return `<h1>book Number ${index+1} <p> <strong>Book Name : </strong> ${book.name}</p> <p><strong>Author Name:</strong> ${book.authorName}</p><p><strong>Book Description : </strong> ${book.bookDescription}</p><p><strong>No.of pages : </strong> ${book.pagesNumber} mins <p> <button onclick="deleteBook(${index})">Delete</button> </p>`});
+    
+     document.getElementById('books').innerHTML = booksDiv.join(' ');
+
+
+  
 }
 function clearInputs(){
     document.getElementById('bookName').value = '';
     document.getElementById('authourName').value = '' ;
     document.getElementById('bookDescription').value ='';
     document.getElementById('pagesNumber').value ='';
+}
+//function created for a delete button
+function deleteBook(index) {
+    // Remove the book from the array using splice
+  let result =  books.splice(index, 1);
+   console.log(result)
+    // Update the displayed books after deletion
+  showbooks();
 }
