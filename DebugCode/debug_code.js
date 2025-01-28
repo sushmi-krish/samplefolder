7db4f505760b5a9a2b7f4ccadb410db12bdc7eb3
sushmi-krish <sushmithakrishnamoorthy>
2025-01-28 09:04:11 -0500

    updated arithmatic operator in debug_code.js

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -5,22 +5,37 @@ function performOperation() {
     //check if inputs are valid numbers
     if(!isNaN(num1) && !isNaN(num2)) {
         //perform operation 
-        let result = multiply(num1,num2);
+        let addValue = add(num1,num2);
+        let divValue = div(num1,num2);
+        let mulValue = multiply(num1,num2);
 //display the result
-      displayResult(result);
+      displayResult(addValue,divValue,mulValue);
     }
     else{
         displayResult('Please enter the vaild numbers');
     }
 }
+function add(a,b){
+    //introduce debugger statement to pause execution 
+    debugger
+    //return  adddition value
+    return a+b;
+}
+function div(a,b){
+    //introduce debugger statement to pause execution 
+     debugger
+     //return the division value 
+    return a/b;
+}
+
  function multiply(a,b){
     //Introduce a debugger statement to pause execution
     debugger;
     //Multiply numbers
     return a*b;
  }
- function displayResult(result){
+ function displayResult(addValue,divValue,mulValue){
     //Display the result in the paragraph element 
     const resultElement = document.getElementById('result')
-     resultElement.textContent = `The result is ${result}`;
+     resultElement.textContent = ;
  }
